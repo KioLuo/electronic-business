b57ec4a536696be2bcc3ccb1fea2d8337abeee9b
qyluo <qyluo1212@foxmail.com>
2017-04-05 22:53:42 +0800

    20170405-22:53

--- src/main/java/com/qyluo/controller/ProjectController.java
+++ src/main/java/com/qyluo/controller/ProjectController.java
@@ -1,6 +1,8 @@
 package com.qyluo.controller;
 
+import com.qyluo.meta.Person;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ProjectController {
     @RequestMapping(value="/index")
-    public String showIndex() {
+    public String showIndex(ModelMap map) {
+        Person user = null;
+        map.addAttribute("user", user);
         return "index";
     }
 }
